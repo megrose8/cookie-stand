22d6d105f651afd235cd740534c3fb42f560ef15
MEGAN BREEN <MBREEN@GICW.ORG>
2023-10-30 16:14:22 -0700

    class 6

---
+++ app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+    const seattle = {
+        locationName : 'seattle',
+        minHourlyCust: 23,
+        maxHourlyCust: 65,
+        customer: [],
+        hourlyCookiesArraySeattle [],
+        
+function randomCookies(){
+        for (let hour= 6; hour <= 18; hour++){
+          let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+            hourlyCookiesArray.push(randomCookies);
+          console.log(`Hour ${hour} ${randomCookies} cookies`)
+        }  
+}
+    seattle.hourlyCookiesArraySeattle();
+    console.log(seattle);
+}
+
+//     const tokyo = {
+//     locationName : 'tokyo',
+//     // minHourlyCust : 3, //set later
+//     // maxHourlyCust : 24, //set later
+//     // avgCookie : 1.2, //set later
+//     hourlyCookiesArrayTokyo:[],
+//     };
+//     let hourlyCookiesArrayTokyo = [];
+//     const minTokyo = 3;
+//     const maxTokyo = 24;
+//         for (let hour= 6; hour <= 18; hour++){
+//          let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+//         hourlyCookiesArray.push(randomCookies);
+//     console.log(`Hour ${hour} ${randomCookies} cookies`)
+// }
+//     tokyo.hourlyCookiesArrayTokyo();
+//     console.log(tokyo);
+
+
+// const dubai = {
+//     locationName : 'dubai',
+//     // minHourlyCust : 11, //set later
+//     // maxHourlyCust : 38, //set later
+//     // avgCookie : 3.7, //set later
+//     hourlyCookiesArrayDubai:[],
+// };
+//     let hourlyCookiesArrayDubai = [];
+//     const minDubai = 11;
+//     const maxDubai = 38;
+//         for (let hour= 6; hour <= 18; hour++){
+//         let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+//         hourlyCookiesArray.push(randomCookies);
+//     console.log(`Hour ${hour} ${randomCookies} cookies`)
+// }
+// dubai.hourlyCookiesArrayDubai();
+// console.log(dubai);
+
+// const paris = {
+//     locationName : 'paris',
+//     // minHourlyCust : 20, //set later
+//     // maxHourlyCust : 38, //set later
+//     // avgCookie : 2.3, //set later
+//     hourlyCookies:[],
+// };
+//     let hourlyCookiesArrayParis = [];
+//     const minParis = 20;
+//     const maxParis = 38;
+//     for (let hour= 6; hour <= 18; hour++){
+//     let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+//       hourlyCookiesArray.push(randomCookies);
+//     console.log(`Hour ${hour} ${randomCookies} cookies`)
+// }
+// paris.hourlyCookiesArrayParis();
+// console.log(paris);
+
+// const lima = {
+//     locationName : 'lima',
+//     // minHourlyCust : 2, //set later
+//     // maxHourlyCust : 16, //set later
+//     // avgCookie : 4.6, //set later
+//     hourlyCookies:[],
+// };
+// let hourlyCookiesArrayLima = [];
+// const minLima = 2;
+// const maxLima = 16;
+//   for (let hour= 6; hour <= 18; hour++){
+//     let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+//       hourlyCookiesArray.push(randomCookies);
+//     console.log(`Hour ${hour} ${randomCookies} cookies`)
+// }
+// lima.hourlyCookiesArrayLima();
+// console.log(lima);
+
+
+//random number generator
+
+let hourlyCookiesArray = [];
+        for (let hour= 6; hour <= 18; hour++){
+          let randomCookies = Math.floor(Math.random() * (max - min + 1) + min);
+            hourlyCookiesArray.push(randomCookies);
+          console.log(`Hour ${hour} ${randomCookies} cookies`)
+        }
+
+
