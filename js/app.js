a2dd4d6e95a49a75e3129af0f34218e233b47f2d
MEGAN BREEN <MBREEN@GICW.ORG>
2023-10-31 16:33:10 -0700

    Edits to JS File and HTML. Constructors have been created, however, table is not rendering

--- js/app.js
+++ js/app.js
@@ -1,115 +1,103 @@
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-const seattle = {
-  location: 'Seattle',
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgCookiesPerSale: 6.3,
-  sales: [],
-  estimate: function () {
-    this.sales = estimateSales(this);
-  },
-}
+//constructor//
 
-const tokyo = {
-  location: 'Tokyo',
-  minCustomers: 3,
-  maxCustomers: 24,
-  avgCookiesPerSale: 1.2,
-  sales: [],
-  estimate: function () {
-    this.sales = estimateSales(this);
-  },
+function LocationSales(location, minCustomers,maxCustomers,avgCookiesPerSale) {
+  this.location = location;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgCookiesPerSale = avgCookiesPerSale;
+  this.sales = this.estimateSales();
 }
+//method//
 
-const dubai = {
-  location: 'Dubai',
-  minCustomers: 11,
-  maxCustomers: 38,
-  avgCookiesPerSale: 3.7,
-  sales: [],
-  estimate: function () {
-    this.sales = estimateSales(this);
-  },
-}
-
-const paris = {
-  location: 'Paris',
-  minCustomers: 20,
-  maxCustomers: 38,
-  avgCookiesPerSale: 2.3,
-  sales: [],
-  estimate: function () {
-    this.sales = estimateSales(this);
-  },
-}
-const lima = {
-  location: 'Lima',
-  minCustomers: 2,
-  maxCustomers: 16,
-  avgCookiesPerSale: 4.6,
-  sales: [],
-  estimate: function () {
-    this.sales = estimateSales(this);
-  },
-}
-
-// initialize sales
-seattle.estimate();
-tokyo.estimate();
-dubai.estimate();
-paris.estimate();
-lima.estimate();
-
-function estimateSales(store) {
+LocationSales.prototype.estimateSales = function() {
   const sales = [];
   for (let i = 0; i < hours.length; i++) {
-    const numCustomers = randomInRange(store.minCustomers, store.maxCustomers);
-    const hourSales = Math.ceil(numCustomers * store.avgCookiesPerSale);
+    const numCustomers = randomInRange(this.minCustomers, this.maxCustomers);
+    // console.log (numCustomers)
+    const hourSales = Math.ceil(numCustomers * this.avgCookiesPerSale);
+    // console.log (hourSales);
     sales.push(hourSales);
   }
   return sales;
 }
 
+// //rendering//
+
+// // LocationSales.prototype.render = function ( ) {
+
+// const containerElem = document.getElementById('salesData');
+
+// // const articleElem = document.createElement('article');
+// // containerElem.appendChild(articleElem);
+
+// const tableElem = document.createElement('table');
+// articleElem.appendChild(tableElem);
+
+// const headerRow = document.createElement('tr');
+// tableElem.appendChild(headerRow);
+
+// const sixAmHeaderCell = document.createElement('th');
+// headerRow.appendChild(sixAmHeaderCelll);
+// sixAmHeaderCell.textContent = "6:00 AM";
+
+// const sevenAmHeaderCell = document.createElement('th');
+// headerRow.appendChild(sevenAmHeaderCell);
+// sevenAmHeaderCell.textContent = "7:00 AM";
+
+// const eightAmHeaderCell = document.createElement('th');
+// headerRow.appendChild(eightAmHeaderCellAmHeaderCell);
+// eightAmHeaderCelll.textContent = "8:00 AM";
+
+// const nineAmHeaderCell = document.createElement('th');
+// headerRow.appendChild(nineAmHeaderCellAmHeaderCell);
+// nineAmHeaderCelll.textContent = "9:00 AM";
+
+
+
+// function render(LocationSales) {
+//   // need an article per cookie stand
+//   const cookieStandArticle = document.createElement('article');
+//   container.appendChild(cookieStandArticle);
+
+//   const heading = document.createElement('h2');
+//   cookieStandArticle.appendChild(heading);
+//   heading.textContent = store.location;
+
+//   const hoursList = document.createElement('ul');
+//   cookieStandArticle.appendChild(hoursList);
+
+//   let totalSold = 0;
+
+//   for (let i = 0; i < store.sales.length; i++) {
+//     const salesItem = document.createElement('li');
+//     hoursList.appendChild(salesItem);
+//     const cookiesSoldThisHour = store.sales[i];
+//     totalSold += cookiesSoldThisHour;
+//     const salesInfo = `${hours[i]}: ${cookiesSoldThisHour} cookies`;
+//     salesItem.textContent = salesInfo;
+//   }
+
+//   // add total line
+//   const totalItem = document.createElement('li');
+//   hoursList.appendChild(totalItem);
+//   const totalInfo = `Total: ${totalSold} cookies sold`;
+//   totalItem.textContent = totalInfo;
+
+// }
+
 function randomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+const seattle = new LocationSales('seattle', 23, 65, 6.3);
+const tokyo = new LocationSales('tokyo', 3, 24, 1.2);
+const dubai = new LocationSales('dubai', 11, 38, 3.7);
+const paris =  new LocationSales('paris', 20, 38, 3.7);
+const lima =  new LocationSales('lima', 2, 16, 4.6);
 
-const container = document.getElementById('root');
-
-function render(store) {
-  // need an article per cookie stand
-  const cookieStandArticle = document.createElement('article');
-  container.appendChild(cookieStandArticle);
-
-  const heading = document.createElement('h2');
-  cookieStandArticle.appendChild(heading);
-  heading.textContent = store.location;
-
-  const hoursList = document.createElement('ul');
-  cookieStandArticle.appendChild(hoursList);
-
-  let totalSold = 0;
-
-  for (let i = 0; i < store.sales.length; i++) {
-    const salesItem = document.createElement('li');
-    hoursList.appendChild(salesItem);
-    const cookiesSoldThisHour = store.sales[i];
-    totalSold += cookiesSoldThisHour;
-    const salesInfo = `${hours[i]}: ${cookiesSoldThisHour} cookies`;
-    salesItem.textContent = salesInfo;
-  }
-
-  // add total line
-  const totalItem = document.createElement('li');
-  hoursList.appendChild(totalItem);
-  const totalInfo = `Total: ${totalSold} cookies sold`;
-  totalItem.textContent = totalInfo;
-
-}
-
-render(seattle);
-render(tokyo);
-render(dubai);
-render(paris);
-render(lima);
+// render(seattle);
+// render(tokyo);
+// render(dubai);
+// render(paris);
+// render(lima);
